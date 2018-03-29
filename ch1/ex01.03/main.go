9c8da1faef74470823c41bf444ffafc1766c6790
Sascha <sascha.lange@dreamlines.com>
2018-03-29 12:44:59 +0200

    Solution excersise 01.03

--- ch1/ex01.03/main.go
+++ ch1/ex01.03/main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 import "os"
+
 import "strings"
+import "time"
+
+type fn func()
 
 func echo1() {
+	fmt.Println("name: echo1")
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
@@ -14,6 +19,7 @@ func echo1() {
 }
 
 func echo2() {
+	fmt.Println("name: echo2")
 	s, sep := "", ""
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
@@ -24,8 +30,20 @@ func echo2() {
 }
 
 func echo3() {
+	fmt.Println("name: echo3")
 	fmt.Println(strings.Join(os.Args[1:], " "))
 }
 
+func bench(f fn) {
+	start := time.Now()
+	f()
+	fmt.Printf("%.10fs elapsed\n", time.Since(start).Seconds())
+}
+
 func main() {
+	flist := []func(){echo1, echo2, echo3}
+
+	for _, f := range flist {
+		bench(f)
+	}
 }
